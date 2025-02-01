b49ad139c8dee62abd1037a8b759e7f4f6afe358
niluzz <33814142+niluzz@users.noreply.github.com>
2025-02-01 14:08:49 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,51 +6,13 @@ sudo pacman -Syu --noconfirm
 
 # Instala dependências básicas
 echo "Instalando dependências básicas..."
-sudo pacman -S --noconfirm git base-devel 
-
-# Instala pacotes do repositório oficial (pacman)
-echo "Instalando pacotes do repositório oficial..."
-sudo pacman -S --noconfirm \
-    file-roller \
-    p7zip \
-    unrar \
-    unzip \ 
-    pacman-contrib \
-    sssd \
-    firefox-i18n-pt-br \
-    discord \
-    telegram-desktop \
-    qbittorrent \
-    bluez-utils \
-    kcalc \
-    clamav \
-    ttf-dejavu-nerd \
-    ttf-hack-nerd \
-    fwupd \
-    libva-mesa-driver \
-    lib32-libva-mesa-driver \
-    mesa-vdpau \
-    lib32-mesa-vdpau \
-    showtime \
-    papers \
-    geary \
-    gnome-firmware \
-    amf-headers \
-    power-profiles-daemon \
-    gnome-boxes \
-    neofetch
+sudo pacman -S --noconfirm git base-devel file-roller p7zip unrar unzip pacman-contrib sssd firefox-i18n-pt-br discord telegram-desktop qbittorrent bluez-utils kcalc clamav 
+ttf-dejavu-nerd ttf-hack-nerd fwupd libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau showtime papers geary gnome-firmware amf-headers power-profiles-daemon
+neofetch
 
 # Instala pacotes do AUR (paru)
 echo "Instalando pacotes do AUR..."
-paru -S --noconfirm \
-    google-chrome \
-    aic94xx-firmware \
-    qed-git ast-firmware \
-    wd719x-firmware \
-    upd72020x-fw \
-    onlyoffice-bin \
-    teamviewer \
-    extension-manager
+paru -S --noconfirm google-chrome aic94xx-firmware qed-git ast-firmware wd719x-firmware upd72020x-fw onlyoffice-bin teamviewer extension-manager
 
 # Habilita e inicia o timer do fwupd
 echo "Ativando o fwupd-refresh.timer..."
