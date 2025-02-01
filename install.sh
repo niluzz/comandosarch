4c3dbb8bedba83a3d80e135789dffe7d1c40d220
niluzz <33814142+niluzz@users.noreply.github.com>
2025-02-01 18:31:59 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,33 +2,59 @@
 
 # Atualiza o sistema
 echo "Atualizando o sistema..."
-sudo pacman -Syu --noconfirm
+if ! sudo pacman -Syu --noconfirm; then
+    echo "Erro ao atualizar o sistema. Verifique sua conexão com a internet."
+    exit 1
+fi
 
 # Instala dependências básicas
 echo "Instalando dependências básicas..."
-sudo pacman -S --noconfirm git base-devel file-roller p7zip unrar unzip pacman-contrib sssd firefox-i18n-pt-br discord telegram-desktop qbittorrent bluez-utils kcalc clamav ttf-dejavu-nerd ttf-hack-nerd fwupd libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau showtime papers geary gnome-firmware amf-headers power-profiles-daemon neofetch
+if ! sudo pacman -S --noconfirm git base-devel file-roller p7zip unrar unzip pacman-contrib sssd firefox-i18n-pt-br discord telegram-desktop qbittorrent bluez-utils kcalc clamav ttf-dejavu-nerd ttf-hack-nerd fwupd libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau showtime papers geary gnome-firmware amf-headers power-profiles-daemon neofetch; then
+    echo "Erro ao instalar dependências básicas."
+    exit 1
+fi
 
 # Instala o paru (AUR helper)
 echo "Instalando o paru..."
-git clone https://aur.archlinux.org/paru.git
+if ! git clone https://aur.archlinux.org/paru.git; then
+    echo "Erro ao clonar o repositório do paru."
+    exit 1
+fi
 cd paru
-makepkg -si --noconfirm
+if ! makepkg -si --noconfirm; then
+    echo "Erro ao instalar o paru."
+    exit 1
+fi
+cd ..
+rm -rf paru
 
 # Instala pacotes do AUR (paru)
 echo "Instalando pacotes do AUR..."
-paru -S --noconfirm google-chrome aic94xx-firmware qed-git ast-firmware wd719x-firmware upd72020x-fw onlyoffice-bin teamviewer extension-manager
+if ! paru -S --noconfirm google-chrome aic94xx-firmware qed-git ast-firmware wd719x-firmware upd72020x-fw onlyoffice-bin teamviewer extension-manager; then
+    echo "Erro ao instalar pacotes do AUR."
+    exit 1
+fi
 
 # Habilita e inicia o timer do fwupd
 echo "Ativando o fwupd-refresh.timer..."
-sudo systemctl enable --now fwupd-refresh.timer
+if ! sudo systemctl enable --now fwupd-refresh.timer; then
+    echo "Erro ao ativar o fwupd-refresh.timer."
+    exit 1
+fi
 
-# Habilitando bluetooth
-echo "Ativando o bluetooth"
-sudo systemctl enable --now bluetooth.service
+# Habilita o Bluetooth
+echo "Ativando o bluetooth..."
+if ! sudo systemctl enable --now bluetooth.service; then
+    echo "Erro ao ativar o bluetooth."
+    exit 1
+fi
 
-# Habilitando teamview
-echo "Ativando o teamview"
-sudo systemctl enable --now teamviewerd.service
+# Habilita o TeamViewer
+echo "Ativando o teamviewer..."
+if ! sudo systemctl enable --now teamviewerd.service; then
+    echo "Erro ao ativar o teamviewer."
+    exit 1
+fi
 
 # Mensagem final
 echo "Instalação concluída!"
