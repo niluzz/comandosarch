be6cb0577c9853b2859763c5a671ff2e770dec4f
niluzz <33814142+niluzz@users.noreply.github.com>
2025-01-31 22:05:36 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Atualiza o sistema
+echo "Atualizando o sistema..."
+sudo pacman -Syu --noconfirm
+
+# Instala dependências básicas
+echo "Instalando dependências básicas..."
+sudo pacman -S --noconfirm git base-devel 
+
+# Instala pacotes do repositório oficial (pacman)
+echo "Instalando pacotes do repositório oficial..."
+sudo pacman -S --noconfirm \
+    file-roller 
+    p7zip \
+    unrar \
+    unzip 
+    pacman-contrib \
+    sssd \
+    firefox-i18n-pt-br \
+    discord \
+    telegram-desktop \
+    qbittorrent \
+    bluez-utils \
+    kcalc \
+    clamav \
+    ttf-dejavu-nerd \
+    ttf-hack-nerd \
+    fwupd \
+    libva-mesa-driver \
+    lib32-libva-mesa-driver \
+    mesa-vdpau \
+    lib32-mesa-vdpau \
+    showtime \
+    papers \
+    geary \
+    gnome-firmware \
+    amf-headers \
+    power-profiles-daemon \
+    gnome-boxes
+
+# Instala pacotes do AUR (yay)
+echo "Instalando pacotes do AUR..."
+paru--noconfirm \
+    google-chrome \
+    aic94xx-firmware \
+    qed-git ast-firmware \
+    wd719x-firmware \
+    upd72020x-fw
+
+# Mensagem final
+echo "Instalação concluída!"
