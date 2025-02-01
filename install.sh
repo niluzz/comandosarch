e062df79d872cdd03d868a29e93e6f9dbf9f8985
niluzz <33814142+niluzz@users.noreply.github.com>
2025-02-01 14:29:43 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,40 +30,5 @@ sudo systemctl enable --now bluetooth.service
 echo "Ativando o teamview"
 sudo systemctl enable --now teamviewerd.service
 
-# Instala o ZSH
-echo "Instalando o ZSH..."
-sudo pacman -S --noconfirm zsh
-
-# Instala o Oh My Zsh
-echo "Instalando o Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Instala o tema Powerlevel10k
-echo "Instalando o tema Powerlevel10k..."
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-
-# Configura o tema Powerlevel10k no ~/.zshrc
-echo "Configurando o tema Powerlevel10k..."
-sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
-
-# Instala plugins para ZSH
-echo "Instalando plugins para ZSH..."
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-
-# Instala o fzf (Fuzzy Finder)
-echo "Instalando o fzf..."
-git clone --depth 1 https://github.com/junegunn/fzf.git
-~/.fzf/install --all --no-update-rc
-
-# Configura os plugins no ~/.zshrc
-echo "Configurando plugins no ~/.zshrc..."
-sed -i 's/^plugins=.*/plugins=(git zsh-syntax-highlighting zsh-autosuggestions fzf zsh-history-substring-search)/' ~/.zshrc
-
-# Recarrega o ZSH
-echo "Recarregando o ZSH..."
-source ~/.zshrc
-
 # Mensagem final
 echo "Instalação concluída!"
