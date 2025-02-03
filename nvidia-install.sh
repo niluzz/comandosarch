3c8db3f9250de3afa912bdcbbd13674190b86210
niluzz <33814142+niluzz@users.noreply.github.com>
2025-02-03 13:38:06 -0300

    Update nvidia-install.sh

--- nvidia-install.sh
+++ nvidia-install.sh
@@ -90,20 +90,6 @@ else
     echo "Aviso: /etc/mkinitcpio.conf não encontrado. Pulando configuração da NVIDIA."
 fi
 
-# Adicionando parâmetros ao kernel
-if [ -f /etc/kernel/cmdline ]; then
-    echo "Adicionando parâmetros NVIDIA ao kernel..."
-    desired_param="nvidia-drm.modeset=1 nvidia_drm.fbdev=1 loglevel=3 quiet splash"
-    
-    if ! grep -q "nvidia-drm.modeset=1" /etc/kernel/cmdline; then
-        echo "$desired_param" | sudo tee -a /etc/kernel/cmdline > /dev/null
-    else
-        echo "Parâmetros NVIDIA já configurados."
-    fi
-else
-    echo "Aviso: /etc/kernel/cmdline não encontrado. Pulando configuração do kernel."
-fi
-
 # Regenera initramfs novamente
 sudo mkinitcpio -P || handle_error "Falha ao regenerar initramfs."
 
