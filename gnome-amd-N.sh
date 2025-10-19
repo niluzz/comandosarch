0f562321e055f2e844ad4c96f4abb19a88e8bf3a
niluzz <33814142+niluzz@users.noreply.github.com>
2025-10-19 11:11:47 -0300

    Add noto-fonts-cjk to font installation
    Added noto-fonts-cjk to the font installation list.

--- gnome-amd-N.sh
+++ gnome-amd-N.sh
@@ -29,7 +29,8 @@ echo ">>> Fontes..."
 sudo pacman -S --needed --noconfirm \
   ttf-firacode-nerd ttf-dejavu-nerd ttf-hack-nerd \
   inter-font noto-fonts noto-fonts-emoji \
-  ttf-montserrat ttf-opensans ttf-roboto
+  ttf-montserrat ttf-opensans ttf-roboto \
+  noto-fonts-cjk
 
 echo ">>> Outros..."
 sudo pacman -S --needed --noconfirm \
