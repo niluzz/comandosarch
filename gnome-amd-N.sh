8a0531b5a90ac8fa268a701048ff1cc96db5765e
niluzz <33814142+niluzz@users.noreply.github.com>
2026-03-03 13:21:04 -0300

    Update gnome-amd-N.sh

--- gnome-amd-N.sh
+++ gnome-amd-N.sh
@@ -10,7 +10,7 @@ echo ">>> Ferramentas básicas..."
 sudo pacman -S --needed --noconfirm \
   git zsh systemd-ukify pacman-contrib \
   file-roller p7zip unrar unzip \
-  fwupd mesa-utils ibus
+  fwupd mesa-utils ibus base-devel
 
 
 echo ">>> Navegadores e comunicação..."
